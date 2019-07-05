8ee17f4fd92ccb9ed422e7f989d758a2cef6c301
Thiago Fasano <Thiago@Fiotec.com>
2019-07-05 14:46:16 -0300

    Front-end inital;

---
+++ frontend/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Project from './pages/Project';
+import Home from './pages/Home';
+
+function Routes(){
+    return (
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/project" component={Project} />
+        </Switch>
+    );
+}
+
+export default Routes;
